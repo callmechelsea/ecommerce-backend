aa6712975dc164313f1f6a13cce41b9fd65e03b9
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-12 18:06:36 -0700

    added producttags

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer value
+      autoIncrement: true, // auto increment
+      primaryKey: true, // primary key
+      allowNull: false, // can't be null
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // integer value
+      references: {
+        model: 'product', // references the product model
+        key: 'id', // references the id column
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // integer value
+      references: {
+        model: 'tag', // references the tag model
+        key: 'id', // references the id column
+      },
+    },  
   },
   {
     sequelize,
