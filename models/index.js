4a75d4a26124b84216d33fa9ab01b39678acf47c
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-12 18:27:35 -0700

    added index.js config

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // foreign key
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // foreign key
+  onDelete: 'CASCADE', // if a category is deleted, delete any associated products
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'product_tag', // through ProductTag
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'product_tag', // through ProductTag
+});
 
 module.exports = {
   Product,
